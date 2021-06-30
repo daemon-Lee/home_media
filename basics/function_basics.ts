9e0e4298514b41dba84ca6eb65e3b4d00f8bd374
duyld <ldduy.edu99@gmail.com>
2021-06-30 10:06:35 +0700

    done type function

--- basics/function_basics.ts
+++ basics/function_basics.ts
@@ -6,8 +6,17 @@ let greet: Function
 greet = () => console.log('hello')
 greet()
 
-const add = (a: number, b: number) => {
+const add = (a: number, b: number, c?: number|string) => {
     console.log(a+b)
-    console.log("first number:",a)
+    console.log("message:",c)
 }
-add(1,4)
+add(1,4,"Done!")
+
+const addDefault = (a: number, b: number, c:number|string = 10) => {
+    console.log("sum:",a+b)
+    console.log("message:",c)
+}
+addDefault(19,29)
+
+const minus = (a: number, b: number): number => a-b
+console.log(minus(29,19))
