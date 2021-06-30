d5b4a78e4643aba076aa19fe9e901131e03020ae
duyld <ldduy.edu99@gmail.com>
2021-06-30 12:43:01 +0700

    Done data_structor

--- basics/data_structor.ts
+++ basics/data_structor.ts
@@ -6,3 +6,17 @@ let numbers = [1,2,3]
 numbers[3] = 4
 
 console.log(numbers)
+
+let person = {
+    name: 'duy',
+    age: 25,
+    isStudent: false
+}
+
+person.name = 'Linh'
+
+person = {
+    name: 'Linh',
+    age: 19,
+    isStudent: true
+}
