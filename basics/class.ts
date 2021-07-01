6e5edafa45ade5bb0dc8ea12b125c9de700939c5
duyld <ldduy.edu99@gmail.com>
2021-07-01 15:57:35 +0700

    type class

---
+++ basics/class.ts
@@ -0,0 +1,23 @@
+// CLASS
+
+export class Employee {
+    public name: string
+    private age: number
+    readonly male: boolean
+
+    constructor(n:string, a:number, m:boolean){
+        this.name = n
+        this.age = a
+        this.male = m
+    }
+
+    print() {
+        return 'name:' + this.name
+    }
+}
+
+const duy = new Employee('duy', 22, true)
+console.log(duy.name)
+console.log(duy.male)
+
+duy.name = 'Duy'
---
+++ basics/module.ts
@@ -0,0 +1,5 @@
+// MODULE
+
+import { Employee } from "./class";
+
+const nam = new Employee('nam', 19, false)
