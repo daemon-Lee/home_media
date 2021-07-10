3369c2ad8e2d1f0a9a7ada0586530449814531ad
duyld <ldduy.edu99@gmail.com>
2021-07-10 16:01:43 +0700

    first steps init

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,26 @@
-import express from "express";
+import express, { response } from "express";
 import compression from "compression";
 import "dotenv/config";
 import path from "path";
 
-console.log(process.env.PORT)
+// Create Express sever
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(compression());
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+app.use(
+    express.static(
+        path.join(__dirname, "../src/static"),
+        {maxAge: 31557600001}
+    )
+);
+
+// Primary app router.
+app.get('/', (req,res) => {
+    res.send("Here is home.")
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+
+const server = app.listen(app.get("port"), () =>{
+    console.log(`App running at http:localhost:${app.get('port')}`)
+})
