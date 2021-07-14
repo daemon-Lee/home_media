4d9f74da9f5e40c4676b8e3f41292484359e76c4
duyld <ldduy.edu99@gmail.com>
2021-07-14 22:47:21 +0700

    update video views

--- src/controllers/video_play.ts
+++ src/controllers/video_play.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import path from "path";
 import fs from "fs";
 
-export const index = (rep: Request, res: Response) => {
+export const player = (req: Request, res: Response) => {
   res.render("fileHandle/video_play", {
     title: "video player",
-
+    video_path: req.query.video_path,
   })
 }
